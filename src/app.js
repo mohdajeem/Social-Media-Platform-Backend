0925f137e5281c1d25c53209d533834519940dcf
mohdajeem <149220624+mohdajeem@users.noreply.github.com>
2025-08-25 01:45:47 +0530

    fixing get/post error

--- src/app.js
+++ src/app.js
@@ -22,8 +22,8 @@ app.use(helmet());
 app.use(cors());
 
 // Body parsing middleware
-app.use(express.json({ limit: "10mb" }));
-app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
 
 // API routes
 app.use("/api/auth", authRoutes);
